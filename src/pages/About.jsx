0c908b162590f3eccf44332462329f7fc10b706e
Katiuska Alicea <katiuskaalicea@gmail.com>
2024-04-21 22:38:26 -0700

    Add Footer component to MainLayout and About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className="mockup-browser border bg-base-300 my-[5rem] mx-auto w-[85%]">
+        <div className="mockup-browser border bg-base-300 my-[5rem] mx-auto w-[85%] min-h-screen">
             <div className="mockup-browser-toolbar">
                 <div className="input">https://yourapp.com</div>
             </div>
-            <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+            <div className="flex justify-center min-h-lvh px-4 py-16 bg-base-200">Hello!</div>
         </div>
     )
 }

c90a68b584605a4afdf6d417191e314bc06d3a6b
Katiuska Alicea <katiuskaalicea@gmail.com>
2024-05-11 23:16:43 -0700

    chore: Add registration functionality to register form component

--- src/components/register/registerForm.jsx
+++ src/components/register/registerForm.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+
 
 const registerForm = () => {
+    const port = 8080
+    const [registration, setRegistration] = useState({
+        email: '',
+        password: ''
+    })
+    console.log(registration)
+
+    const handleChange = (e) => {
+        setRegistration({ ...registration, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post(`http://localhost:${port}/api/users/register`, registration)
+            .then(response => {
+                console.log(response)
+
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        setRegistration({
+            email: '',
+            password: ''
+        })
+
+    }
+
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,18 +40,18 @@ const registerForm = () => {
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body">
+                    <form className="card-body" onSubmit={handleSubmit}>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input name='email' type="email" value={registration.email} onChange={handleChange} placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="password" name="password" onChange={handleChange} value={registration.password} placeholder="password" className="input input-bordered" required />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>

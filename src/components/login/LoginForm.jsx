557abb72abd1d87dc11bce7298135bcead6f6e98
Katiuska Alicea <katiuskaalicea@gmail.com>
2024-05-15 23:45:48 -0700

    chore: Update login form component with state management and API integration

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const LoginForm = () => {
-    return (
-        <div>LoginForm</div>
-    )
-}
+    const [login, setLogin] = useState({
+        email: '',
+        password: '',
+    });
 
-export default LoginForm
+    const navigate = useNavigate();
+    const navigateToDashboard = () => {
+        navigate('/dashboard');
+    };
+
+    const handleChange = (e) => {
+        setLogin({ ...login, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post('http://localhost:8080/api/users/login', login)
+            .then((response) => {
+                console.log(response);
+                navigateToDashboard();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        setLogin({
+            email: '',
+            password: '',
+        });
+    };
+
+    return (
+        <div className='hero min-h-screen bg-base-200'>
+            <div className='hero-content flex-col lg:flex-row-reverse'>
+                <div className='text-center lg:text-left'>
+                    <h1 className='text-5xl font-bold'>Login now!</h1>
+                    <p className='py-6'>
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+                        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+                        a id nisi.
+                    </p>
+                </div>
+                <div className='card shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
+                    <form
+                        onSubmit={handleSubmit}
+                        className='card-body'
+                    >
+                        <div className='form-control'>
+                            <label className='label'>
+                                <span className='label-text'>Email</span>
+                            </label>
+                            <input
+                                type='email'
+                                name='email'
+                                value={login.email}
+                                onChange={handleChange}
+                                placeholder='email'
+                                className='input input-bordered'
+                                required
+                            />
+                        </div>
+                        <div className='form-control'>
+                            <label className='label'>
+                                <span className='label-text'>Password</span>
+                            </label>
+                            <input
+                                type='password'
+                                name='password'
+                                value={login.password}
+                                onChange={handleChange}
+                                placeholder='password'
+                                className='input input-bordered'
+                                required
+                            />
+                            <label className='label'>
+                                <a
+                                    href='#'
+                                    className='label-text-alt link link-hover'
+                                >
+                                    Forgot password?
+                                </a>
+                            </label>
+                        </div>
+                        <div className='form-control mt-6'>
+                            <button className='btn btn-primary'>Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LoginForm;
